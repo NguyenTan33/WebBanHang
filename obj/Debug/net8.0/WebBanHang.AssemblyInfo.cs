d11f5e583a6fd37a87041eaf04f7878bc630cdf3
tqt20004 <thangtu0440@gmail.com>
2026-04-03 00:00:52 +0700

    z
    z

--- obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0be850f513ba5a77f36249b1db1235fa41c03f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5296fd0b38ad9a4f33c408cc9a76879058c794ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
